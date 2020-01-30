d4ef43e2eabeaf6750926121cc57d156693f5fc7
go shibata <warahiko@gmail.com>
2020-01-30 22:57:10 +0900

    Update: use data bind in MainActivity

--- app/src/main/java/com/example/go/memoexpensesapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/go/memoexpensesapplication/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.go.memoexpensesapplication.activity
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.go.memoexpensesapplication.R
+import com.example.go.memoexpensesapplication.databinding.ActivityMainBinding
 import com.example.go.memoexpensesapplication.fragment.ExpenseListFragment
 import com.example.go.memoexpensesapplication.fragment.TagListFragment
 
@@ -10,18 +12,22 @@ class MainActivity :
     AppCompatActivity(),
     ExpenseListFragment.OnFragmentInteractionListener {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
         supportFragmentManager.beginTransaction()
-            .replace(R.id.activity_main_container, ExpenseListFragment.newInstance())
+            .replace(binding.container.id, ExpenseListFragment.newInstance())
             .commit()
     }
 
     override fun onTransitionTagList() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.activity_main_container, TagListFragment.newInstance())
+            .replace(binding.container.id, TagListFragment.newInstance())
             .addToBackStack(null)
             .commit()
     }
