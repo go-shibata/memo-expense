5839278f097e8149a02ecd3a359c6c7632b72b50
go shibata <warahiko@gmail.com>
2020-03-08 14:16:58 +0900

    Refactor: inject FragmentLoginViewModel with constructor arguments

--- app/src/main/java/com/example/go/memoexpensesapplication/viewmodel/FragmentLoginViewModel.kt
+++ app/src/main/java/com/example/go/memoexpensesapplication/viewmodel/FragmentLoginViewModel.kt
@@ -2,7 +2,6 @@ package com.example.go.memoexpensesapplication.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.go.memoexpensesapplication.di.component.LoginComponent
 import com.example.go.memoexpensesapplication.dispatcher.LoginDispatcher
 import com.example.go.memoexpensesapplication.navigator.FragmentLoginNavigator
 import io.reactivex.Flowable
@@ -11,38 +10,34 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.processors.PublishProcessor
 import javax.inject.Inject
 
-class FragmentLoginViewModel : ViewModel() {
+class FragmentLoginViewModel @Inject constructor(
+    dispatcher: LoginDispatcher
+) : ViewModel() {
 
-    private lateinit var navigator: FragmentLoginNavigator
-
-    @Inject
-    lateinit var dispatcher: LoginDispatcher
+    private var navigator: FragmentLoginNavigator? = null
 
     val mail: MutableLiveData<String> = MutableLiveData()
     val password: MutableLiveData<String> = MutableLiveData()
 
-    private lateinit var login: Disposable
+    private val login: Disposable
     private val _authenticationFail = PublishProcessor.create<Unit>()
     val authenticationFail: Flowable<Unit> = _authenticationFail
 
-    fun setNavigator(navigator: FragmentLoginNavigator) {
-        this.navigator = navigator
-    }
-
-    fun inject(loginComponent: LoginComponent) {
-        loginComponent.inject(this)
-        subscribe()
-    }
-
-    private fun subscribe() {
+    init {
         login = dispatcher.onLogin
             .map { action -> action.data }
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { user -> navigator.onLoggedIn(user) }
+            .subscribe { user ->
+                navigator?.onLoggedIn(user) ?: throw RuntimeException("Navigator must be set")
+            }
         dispatcher.onAuthenticationFail
             .map { action -> action.data }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(_authenticationFail)
+    }
+
+    fun setNavigator(navigator: FragmentLoginNavigator) {
+        this.navigator = navigator
     }
 
     override fun onCleared() {
