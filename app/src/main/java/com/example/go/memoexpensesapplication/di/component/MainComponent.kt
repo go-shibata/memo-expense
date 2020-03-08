8f2d01f8ce504e2e877e11fb39862add0620c8ef
go shibata <warahiko@gmail.com>
2020-03-08 14:23:53 +0900

    Refactor: inject MainDispatcher

--- app/src/main/java/com/example/go/memoexpensesapplication/di/component/MainComponent.kt
+++ app/src/main/java/com/example/go/memoexpensesapplication/di/component/MainComponent.kt
@@ -1,16 +1,15 @@
 package com.example.go.memoexpensesapplication.di.component
 
 import com.example.go.memoexpensesapplication.actioncreator.MainActionCreator
-import com.example.go.memoexpensesapplication.fragment.MainFragment
 import com.example.go.memoexpensesapplication.di.module.DatabaseModule
-import com.example.go.memoexpensesapplication.di.module.MainModule
 import com.example.go.memoexpensesapplication.di.module.PreferencesModule
+import com.example.go.memoexpensesapplication.fragment.MainFragment
 import com.example.go.memoexpensesapplication.viewmodel.FragmentMainViewModel
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [DatabaseModule::class, PreferencesModule::class, MainModule::class])
+@Component(modules = [DatabaseModule::class, PreferencesModule::class])
 interface MainComponent {
     fun inject(actionCreator: MainActionCreator)
     fun inject(mainViewModel: FragmentMainViewModel)
