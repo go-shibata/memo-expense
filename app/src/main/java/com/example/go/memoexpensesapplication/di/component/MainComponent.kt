3ad355f39516a485e4f93360215ebdb489c21532
go shibata <warahiko@gmail.com>
2020-03-08 14:40:47 +0900

    Refactor: inject Database

--- app/src/main/java/com/example/go/memoexpensesapplication/di/component/MainComponent.kt
+++ app/src/main/java/com/example/go/memoexpensesapplication/di/component/MainComponent.kt
@@ -1,7 +1,6 @@
 package com.example.go.memoexpensesapplication.di.component
 
 import com.example.go.memoexpensesapplication.actioncreator.MainActionCreator
-import com.example.go.memoexpensesapplication.di.module.DatabaseModule
 import com.example.go.memoexpensesapplication.di.module.PreferencesModule
 import com.example.go.memoexpensesapplication.fragment.MainFragment
 import com.example.go.memoexpensesapplication.viewmodel.FragmentMainViewModel
@@ -9,7 +8,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [DatabaseModule::class, PreferencesModule::class])
+@Component(modules = [PreferencesModule::class])
 interface MainComponent {
     fun inject(actionCreator: MainActionCreator)
     fun inject(mainViewModel: FragmentMainViewModel)
